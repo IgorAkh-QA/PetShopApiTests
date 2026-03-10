c5dd9532be05cf8e52ba1554adee4bbb5fae9c15
IgorAkh <pe3k4bo0@ya.ru>
2026-03-10 17:16:47 +0300

    added cycle with wrong status

--- src/test/java/tests/TestPet.java
+++ src/test/java/tests/TestPet.java
@@ -109,7 +109,8 @@ public class TestPet {
     @CsvSource({
             "581, Pes, available",
             "582, Kot, pending",
-            "583, Ryba, sold"
+            "583, Ryba, sold",
+            "584, Slon, killed"
     })
     @Feature("Pet")
     @Severity(SeverityLevel.CRITICAL)
